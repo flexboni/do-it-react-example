d0776e3538909e6276b34d02488ba8689fc4bdd3
flexboni <kgb8594@gmail.com>
2021-03-16 16:45:34 +0900

    04 스토리북 action 추가

--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Input from '../03/Input';
 
 storiesOf('Input', module)
   .add('basic setting', () => <Input name="name" />)
-  .add('lable example', () => <Input name="name" lable="name" />);
+  .add('lable example', () => <Input name="name" lable="name" />)
+  .add('onChange example', () => <Input name="name" onChange={action('onChange 이벤트 발생')} />);
