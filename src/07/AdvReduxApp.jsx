1c309dcaf6f8d3e22ddd291d3e450cc791bf7810
flexboni <kgb8594@gmail.com>
2021-03-17 15:47:38 +0900

    07-3 그래프 데이터베이스 도입하기 - 1
    객체 배열 형태의 자료를 스토어에 저장하도록 앱 수정하기

--- src/07/AdvReduxApp.jsx
+++ src/07/AdvReduxApp.jsx
@@ -2,23 +2,17 @@ import React, { PureComponent } from 'react';
 import configureStore from './configureStore';
 import { Provider } from 'react-redux';
 
-import { setLoading, resetLoading } from './actions/loadingActions';
-import { setUser } from './actions/userActions';
+import { setCollection } from './actions/collectionActions';
 
 class AdvReduxApp extends PureComponent {
   store = configureStore({ loading: false });
 
   componentDidMount() {
-    this.store.dispatch(setLoading(true));
-    this.store.dispatch(resetLoading());
     this.store.dispatch(
-      setUser({
-        type: 'SET_USER',
-        payload: {
-          name: 'Kim',
-          age: 30,
-        },
-      }),
+      setCollection([
+        { id: 1, name: 'Kim', age: 30 },
+        { id: 2, name: 'Park', age: 27 },
+      ]),
     );
   }
 
--- src/07/reducers/index.js
+++ src/07/reducers/index.js
@@ -1,8 +1,10 @@
 import loading from './loadingReducer';
 import user from './userReducer';
+import collection from './collectionReducer';
 
 // webpack이 index 파일을 자동으로 참조함
 export default {
   loading,
   user,
+  collection,
 };
