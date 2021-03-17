20555bb3f573cbca1e96642926c175a6bc5be6f0
flexboni <kgb8594@gmail.com>
2021-03-17 14:23:17 +0900

    07-2 액션과 리듀서의 관계 알아보기 - 1
         리듀서 구현하고 실행 해보기

--- src/07/ReduxApp.jsx
+++ src/07/ReduxApp.jsx
@@ -2,9 +2,34 @@ import React, { PureComponent } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+const reducer = (state, action) => {
+  const { type, payload } = action; // 구조 분해 할당
+  switch (
+    type // 액션 타입에 맞는 리듀서 실행
+  ) {
+    case 'SET_LOADING': {
+      return {
+        ...state,
+        loading: payload,
+      };
+    }
+    case 'RESET_LOADING': {
+      return { ...state, loading: false }; // loading value 초기화
+    }
+    case 'SET_USER': {
+      return {
+        ...state,
+        user: payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
 class ReduxApp extends PureComponent {
   store = createStore(
-    state => state,
+    reducer, // 리듀서 설정
     { loading: false, name: 'do it react' },
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
@@ -13,6 +38,17 @@ class ReduxApp extends PureComponent {
     this.store.dispatch({
       type: 'SET_LOADING',
       payload: true,
+    });
+    this.store.dispatch({
+      type: 'RESET_LOADING',
+    });
+    this.store.dispatch({
+      type: 'SET_USER',
+      payload: {
+        // user 정보 추가
+        name: 'Kim',
+        age: 30,
+      },
     });
   }
 
