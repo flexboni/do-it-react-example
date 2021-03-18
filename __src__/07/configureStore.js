da12e871e252c104b0b372fe2b0a4a544cdbe730
flexboni <kgb8594@gmail.com>
2021-03-18 18:03:46 +0900

    가상 화폐 프로젝트 기본 컴포넌트 생성

---
+++ __src__/07/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+export default initStates =>
+  createStore(combineReducers(reducers), initStates, composeWithDevTools(applyMiddleware(thunk)));
